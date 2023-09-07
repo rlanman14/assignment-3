3f036e0ce26f16b980435940405c00c6328ed74c
rlanman14 <ryan.lanman@tamu.edu>
2023-09-07 17:09:23 -0500

    comments

--- ryan.py
+++ ryan.py
@@ -1,6 +1,10 @@
 import urllib.request
+#import request module (on PSL)
 
 url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+#specify url
 filename = 'log.txt'
+#name dowloaded file
 
+#download file
 urllib.request.urlretrieve(url, filename)
